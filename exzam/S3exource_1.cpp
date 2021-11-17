b732c97a6ac7673e55ffb4edc77173b5f1dab08f
listok195 <72712408+listok195@users.noreply.github.com>
2021-11-17 21:15:05 +0300

    еапр

--- exzam/S3exource_1.cpp
+++ exzam/S3exource_1.cpp
@@ -1,44 +1,8 @@
-/*#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <conio.h>
 #include <string>
 #include <vector>
-
-/*template <typename T>
-void countMenuSize(int& counter, T t)
-{
-	counter++;
-}
-
-template <typename T, typename ...Args>
-void countMenuSize(int& counter, T t, Args... args)
-{
-
-	counter++;
-	countMenuSize(counter, args...);
-}
-
-template <typename T>
-void print(int& position, int& counter, T t)
-{
-	if (counter == position)
-		std::cout << "=" << t << "=" << std::endl;
-	else
-		std::cout << " " << t << std::endl;
-	counter++;
-}
-
-template <typename T, typename ...Args>
-void print(int& position, int& counter, T t, Args ...arg)
-{
-	if (counter == position)
-		std::cout << "=" << t << "=" << std::endl;
-	else
-		std::cout << " " << t << std::endl;
-	counter++;
-	print(position, counter, arg...);
-}*/
-/*
 class songs
 {
 public:
@@ -127,31 +91,6 @@ public:
 int main()
 {
 	system("chcp 1251");
-	/*int current_position = 0, menu_size = 0, count = 0;
-	countMenuSize(menu_size, "New Game", "Exit");
-	do
-	{
-		system("cls");
-		count = 0;
-		print(current_position, count, "Новая игра", "Exit");
-		switch (_getch())
-		{
-		case 72:
-			if (current_position > 0)
-				current_position--;
-			else
-				current_position = menu_size - 1;
-			break;
-		case 80:
-			if (current_position < menu_size - 1)
-
-				current_position++;
-			else
-				current_position = 0;
-			break;
-
-		}
-	} while (true);
 	int size, choose, posit = 0;
 	std::vector <songs> songs;
 	std::string hou;
@@ -194,4 +133,3 @@ int main()
 }
 
 
-*/
--- exzam/amyatce1.cpp
+++ exzam/amyatce1.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 #include <ctime>
+void printV(std::vector<std::vector<int>> nool)
+{
+    for (int i = 0; i < nool.size(); i++)
+    {
+        for (int j = 0; j < nool.size(); j++)
+        {
+            std::cout << nool[i][j] << "\t";
+
+        }
+        std::cout << std::endl << std::endl;;
+    }
+}
 int main()
 {
     system("chcp 1251");
     system("pause");
     system("cls");
-    int choose, a, b, n;
+    srand((int)time(NULL));
+    int choose, X, Y, n, chec, polX, polY, polX1, polY1, schet = 0;
     do
     {
         std::cout << "\nВыберите размер поля\n1.2х2\n2.4х4\n3.6х6\n";
@@ -15,21 +28,21 @@ int main()
         {
         case 1:
         {
-            a = 2;
-            b = 2;
-           
+            X = 2;
+            Y = 2;
+
         }break;
         case 2:
         {
-            a = 4;
-            b = 4;
-           
+            X = 4;
+            Y = 4;
+
         }break;
         case 3:
         {
-            a = 6;
-            b = 6;
-           
+            X = 6;
+            Y = 6;
+
         }break;
         default:
         {
@@ -37,47 +50,93 @@ int main()
         }break;
         }
     } while (choose != 1 && choose != 2 && choose != 3);
-    
-    std::vector<std::vector<int>> ivec(a);
-    std::vector<std::vector<int>> boba(a);
-    n = (a * b) / 2;
+    std::vector<std::vector<int>> ivec(X);
+    std::vector<std::vector<int>> nool(X);
+    n = (X * Y) / 2;
     std::vector<int> vecr(n);
-    for (auto i = 0; i < a; ++i)
+    chec = X * Y;
+    for (int i = 0; i < vecr.size(); i++)
     {
-        ivec[i].resize(b);
-        for (auto j = 0; j < b; ++j)
-            ivec[i][j] = 0;
+        vecr.at(i) = i + 1;
+    }
+    int i_r, j_r;
+    for (auto i = 0; i < X; ++i)
+    {
+        ivec[i].resize(Y);
+        for (auto j = 0; j < Y; ++j)
+            ivec[i][j] = -1;
     }
     for (auto i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        if (chec != 0)
         {
-            while (ivec.at[a][b] != 0)
+            do
             {
-                ivec.push_back = rand() % a + 1;
-                ivec.push_back = rand() % b + 1;
-            }
-            ivec[i][j] = vecr[i];
-            while (ivec.at[a][b] != 0)
+                i_r = rand() % X;
+                j_r = rand() % Y;
+            } while (ivec.at(i_r).at(j_r) != -1);
+            ivec.at(i_r).at(j_r) = vecr[i];
+            chec--;
+            do
             {
-                ivec.push_back = rand() % a + 1;
-                ivec.push_back = rand() % b + 1;
-               
-            }
-            ivec[i][j] = vecr[i];
+                i_r = rand() % X;
+                j_r = rand() % Y;
+            } while (ivec.at(i_r).at(j_r) != -1);
+            ivec.at(i_r).at(j_r) = vecr[i];
+            chec--;
         }
     }
-    for (auto i = 0; i < a; ++i)
+    for (auto i = 0; i < X; ++i)
     {
-        boba[i].resize(b);
-        for (auto j = 0; j < b; ++j)
-            boba[i][j] = 0;
+        nool[i].resize(Y);
+        for (auto j = 0; j < Y; ++j)
+            nool[i][j] = 0;
     }
+    do
+    {
+        system("pause");
+        system("cls");
+        printV(nool);
+        std::cout << "\nВыберите позицию по X and Y\n";
+        std::cin >> polX;
+        std::cout << "\n";
+        std::cin >> polY;
+        polX--;
+        polY--;
+        nool.at(polX).at(polY) = ivec.at(polX).at(polY);
+        printV(nool);
+        std::cout << "\nВыберите позицию по X and Y\n";
+        std::cin >> polX1;
+        std::cout << "\n";
+        std::cin >> polY1;
+        
+        polX1--;
+        polY1--;
+        if (polX == polX1 && polY == polY1)
+        {
+            std::cout << "Вы не можете выбрать одно и то же положение повторно";
+            break;
+        }
+        else
+        {
+            schet++;
+            nool.at(polX1).at(polY1) = ivec.at(polX1).at(polY1);
+            printV(nool);
+        }
+        if (nool.at(polX).at(polY) == nool.at(polX1).at(polY1))
+        {
+            std::cout << "Верно!\n";
+            nool.at(polX).at(polY) = 404;
+            nool.at(polX1).at(polY1) = 404;
+            printV(nool);
+            std::cout << "\n                               " << schet << " попытка\n404 - угаданные карты";
+        }
+        else
+        {
+            std::cout << "Неверно, повторите попытку                    " << schet << " попытка\n404 - угаданные карты";
+        }
+        break;
 
-    for (int i = 0; i < a; i++)
-    {
-        for (int j = 0; j < b; j++)
-            std::cout << boba[i][j] << " ";
-        std::cout << std::endl;
-    }
+    } while (choose != 0);
 }
+*/
