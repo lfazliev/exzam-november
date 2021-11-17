f0c20dd945d55fdd2487fa19aca9aef183d94c6d
listok195 <listok195@gmail.com>
2021-11-17 03:34:25 +0300

    р

--- exzam/S3exource_1.cpp
+++ exzam/S3exource_1.cpp
@@ -38,7 +38,7 @@ void print(int& position, int& counter, T t, Args ...arg)
 	counter++;
 	print(position, counter, arg...);
 }*/
-class Songs
+class songs
 {
 public:
 	std::string text;
@@ -46,63 +46,81 @@ public:
 	std::string actor;
 	std::string age;
 
-	void addKL()
+	void add()
 	{
-		std::cin >> text;
-		std::cout << "Введите название песни - ";
-		std::cin >> name;
-		std::cout << "Введите исполнителя - ";
-		std::cin >> actor;
-		std::cout << "Введите год выпуска - ";
-		std::cin >> age;
-		std::cout << "Песня добавлена...\n\n";
-	}
-	void addFL(std::string hou)
-	{
-		setlocale(LC_ALL, "rus");
-		
-		std::ifstream file(hou);
-		if (!file)
+		int choose;
+		do
 		{
-			std::cout << "Файл не открыт\n\n";
-		}
-		else
-		{
-			std::cout << "Все ОК! Файл открыт!\n\n";
-		}
-		while (file)
-		{
-			std::getline(file, text);
-			std::cout << text << "\n";
-		}
-		std::cout << "Введите название песни - ";
-		std::cin >> name;
-		std::cout << "Введите исполнителя - ";
-		std::cin >> actor;
-		std::cout << "Введите год выпуска - ";
-		std::cin >> age;
-		std::cout << "Песня добавлена...\n\n";
-	}
-	
-	void print(int a, std::string hou)
-	{
-		system("chcp 1251");
-		if (a = 0)
-		{	
-			std::cout << text;
-		}
-		else
-		{
-			std::ifstream file(hou);
-			while (file)
+			std::cout << "1.С клавиатуры\n2.Из файла\n";
+			std::cin >> choose;
+			switch (choose)
 			{
-				std::getline(file, text);
-				std::cout << text << "\n";
+			case 2:
+			{
+				std::string hou;
+				std::cout << "Укажите путь к файлу - ";
+				std::cin.ignore();
+				std::getline(std::cin, hou);
+				std::ifstream file(hou);
+				if (!file)
+				{
+					std::cout << "Файл не открыт\n\n";
+				}
+				else
+				{
+					std::cout << "Все ОК! Файл открыт!\n\n";
+				}
+				while (file)
+				{
+					std::getline(file, text);
+					std::cout << text << std::endl;
+				}
+				break;
 			}
-		}
-		std::cout << "Название - " << name << "\n";
-		std::cout << "Исполнитель: " << actor << "\n";
-		std::cout << "Год - " << age << "\n";
+			case 1:
+			{
+				std::cin >> text;
+				break;
+			}
+			default:
+				std::cout << "error";
+				break;
+			}
+			std::cout << "Введите название песни - ";
+			std::cin >> name;
+			std::cout << "\n";
+			std::cout << "Введите исполнителя - ";
+			std::cin >> actor;
+			std::cout << "\n";
+			std::cout << "Введите год выпуска - ";
+			std::cin >> age;
+			std::cout << "\n";
+			std::cout << "Песня добавлена...\n";
+			choose = 0;
+		} while (choose != 0);
+	}
+	void print()
+	{
+		std::cout << text << "\n";
+		std::cout << name << "\n";
+		std::cout << actor << "\n";
+		std::cout << age << std::endl;
+	}
+	void edit()
+	{
+
+	}
+	void save()
+	{
+		
+	}
+	void findautor()
+	{
+
+	}
+	void find()
+	{
+
 	}
 };
 int main()
@@ -133,11 +151,11 @@ int main()
 
 		}
 	} while (true);*/
-	int size, choose, chase, a = 0, posit = 0;
-	std::vector <Songs> songs;
+	int size, choose, posit = 0;
+	std::vector <songs> songs;
 	std::string hou;
 	do
-	{	
+	{
 		system("pause");
 		system("cls");
 		std::cout << "1.Добавить\n2.Вывести\n";
@@ -150,39 +168,9 @@ int main()
 			std::cout << "Укажите сколько текстов вы хотите добавить.. \n";
 			std::cin >> size;
 			songs.resize(size);
-			std::cout << "1.Из файла\n2.С клавиатуры\n3.Назад\n";
-			std::cin >> chase;
-			switch (chase)
+			for (int i = 0; i < songs.size(); i++)
 			{
-			case 1:
-			{
-				for (int i = 0; i < songs.size(); i++)
-				{
-					std::getline(std::cin, hou);
-					a = 1;
-					
-					songs.at(i).addFL(hou);
-				}
-				break;
-			}
-			case 2:
-			{
-				for (int i = 0; i < songs.size(); i++)
-					songs.at(i).addKL();
-				break;
-			}
-			case 3:
-			{
-				std::cout << "Сколько текстов вы хотите добавить - \n";
-				std::cin >> size;
-				songs.resize(size);
-				break;
-			}
-			default:
-			{
-				std::cout << "Не верно";
-				break;
-			}
+				songs.at(i).add();
 			}
 			break;
 		}
@@ -192,7 +180,7 @@ int main()
 				std::cout << "Не добавлено ни одного текста..\n";
 			else
 				for (int i = 0; i < songs.size(); i++)
-					songs.at(i).print(a, hou);
+					songs.at(i).print();
 			break;
 		}
 		default:
@@ -201,7 +189,7 @@ int main()
 			break;
 		}
 		}
-	}while (choose != 0);
+	} while (choose != 0);
 }
 
 
