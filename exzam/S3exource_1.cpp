1abfece17c5be3b09d47348b9a5f31b49c62b3ec
listok195 <listok195@gmail.com>
2021-11-18 07:38:18 +0300

    п

--- exzam/S3exource_1.cpp
+++ exzam/S3exource_1.cpp
@@ -20,7 +20,7 @@ public:
 			system("cls");
 			std::cout << "1.С клавиатуры\n2.Из файла\nКак вы хотите добавить текст песни? - ";
 			std::cin >> choose;
-			if (choose = 0)
+			if (choose == 0)
 			{
 				choose--;
 			}
@@ -42,9 +42,8 @@ public:
 					std::cout << "Все ОК! Файл открыт!\n\n";
 					choose = 0;
 				}
-				while (file)
+				while (getline(file, text))
 				{
-					std::getline(file, text);
 					std::cout << text << std::endl;
 					text_sum = text_sum + text + "\n";
 				}
@@ -55,11 +54,12 @@ public:
 				std::cout << "Введите текст\n";
 				std::cin.ignore();
 				std::getline(std::cin, text_sum);
-				break;
+				text_sum = text_sum + "\n";
 				choose = 0;
+				break;
 			}
 			default:
-				std::cout << "error";
+				std::cout << "Нет такого варианта ответа...\n";
 				break;
 			}
 		} while (choose != 0);
@@ -74,21 +74,30 @@ public:
 		std::cout << "\n";
 		std::cout << "Песня добавлена...\n";
 	}
-	void print()
+	void print(int a)
 	{
-		std::cout << "Текст:\n" << text_sum << "\n";
+		std::cout << "\n#" << a << "\nТекст:\n" << text_sum << "\n";
 		std::cout << "Название - " << name << "\n";
 		std::cout << "Исполнитель - " << actor << "\n";
 		std::cout << "Год - " << age << "\n";
 	}
 	void edit()
 	{
-
+		std::cout << "Введите текст\n";
+		std::cin.ignore();
+		std::getline(std::cin, text_sum);
+		text_sum = text_sum + "\n";
 	}
 	void save()
 	{
-		
+		std::ofstream out("txt.txt", std::ios::app);
+		if (out.is_open())
+		{
+			out.write((char*)&text, sizeof(text));
+		}
+		out.close();
 	}
+
 	void findautor()
 	{
 
@@ -101,14 +110,14 @@ public:
 int main()
 {
 	system("chcp 1251");
-	int size = 0, choose, posit = 0,s2;
-	std::vector <songs> songs;
+	int size = 0, choose, posit = 0, s2, a = 0, num, j = 0;
+	std::vector <songs> song;
 	std::string hou;
 	do
 	{
 		system("pause");
 		system("cls");
-		std::cout << "1.Добавить\n2.Вывести\nЧто необходимо сделать? - ";
+		std::cout << "1.Добавить\n2.Вывести\n3.Удалить текст\n4.Изменить текст\n5.Сохранение текста в файл\nЧто необходимо сделать? - ";
 		std::cin >> choose;
 		if (choose == 0)
 			choose--;
@@ -121,11 +130,12 @@ int main()
 			std::cin >> s2;
 			if (s2 > 0)
 			{
-				size = size + s2;
-				songs.resize(size);
+				size = song.size() + s2;
+				song.resize(size);
 				for (int i = 0; i < s2; i++)
 				{
-					songs.at(i).add();
+					song.at(a).add();
+					a++;
 				}
 				break;
 			}
@@ -134,20 +144,111 @@ int main()
 		}
 		case 2:
 		{
-			if (posit == 0)
+			if (song.size() == 0)
 				std::cout << "Не добавлено ни одного текста..\n";
 			else
-				for (int i = 0; i < songs.size(); i++)
-					songs.at(i).print();
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
 			break;
 		}
+		case 3:
+		{
+			if (song.size() == 0)
+				std::cout << "Не добавлено ни одного текста..\n";
+			else
+			{
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
+				std::cout << "\nКакой текст вы хотите удалить? - ";
+				std::cin >> num;
+				num--;
+				if (song.size() > num and num >= 0)
+				{
+					auto iter = song.cbegin();
+					song.erase(iter + num);
+					a--;
+					std::cout << "\nУдалено\n";
+				}
+				else
+				{
+					std::cout << "\nНедопустимое значение!" << std::endl;
+				}
+				break;
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
+			}
+		}break;
+		case 4:
+		{
+			if (song.size() == 0)
+				std::cout << "Не добавлено ни одного текста..\n";
+			else
+			{
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
+				std::cout << "\nКакой текст вы хотите изменить? - ";
+				std::cin >> num;
+				num--;
+				if (song.size() > num and num >= 0)
+				{
+					song.at(num).edit();
+					std::cout << "\nИзменено\n";
+				}
+				else
+				{
+					std::cout << "\nНедопустимое значение!" << std::endl;
+				}
+				break;
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
+			}
+		}break;
+		case 5:
+		{
+			if (song.size() == 0)
+				std::cout << "Не добавлено ни одного текста..\n";
+			else
+			{
+				for (int i = 0; i < song.size(); i++)
+				{
+					j = i + 1;
+					song.at(i).print(j);
+				}
+				std::cout << "\nКакой текст вы хотите сохрнаить? - ";
+				std::cin >> num;
+				num--;
+				if (song.size() > num and num >= 0)
+				{
+					song.at(num).save();
+					std::cout << "\nСохранено!\n";
+				}
+				else
+				{
+					std::cout << "\nНедопустимое значение!" << std::endl;
+				}
+			}
+		}break;
 		default:
 		{
 			std::cout << "Нет такого варианта...\n";
 			break;
 		}
 		}
+
 	} while (choose != 0);
 }
-
-
